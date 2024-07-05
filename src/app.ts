b92936738537d5c4829c74b843b13ff141844f62
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-05 10:34:31 -0700

    feat: setup complete

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
-import express from "express";
+import express, { Router } from "express";
 const app = express();
 import cors from "cors";
 import morgan from "morgan";
 import { defaultMiddleware, errorMiddleware } from "./core/index.middleware";
 import { corsOptions } from "./config/cors";
+import { ROUTES } from "./constants/routes";
 
 app.use(cors(corsOptions));
 app.use(morgan("dev"));
-app.use("/api", defaultMiddleware);
+
+
+// ATTACH OTHER ROUTES TO APIROUTES
+const apiRoutes = Router();
+
+
+
+// DO NOT TOUCH >>>>>>>>
+app.get(ROUTES.BASE, defaultMiddleware);
+app.use(ROUTES.BASE, apiRoutes);
 app.use(errorMiddleware);
 export default app;
