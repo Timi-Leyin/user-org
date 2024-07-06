4092456aef56c310bfb0d9b30df0dd14df96f57e
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-06 14:11:04 -0700

    setup production

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,6 +1,6 @@
 export const ENV = {
   NODE_ENV: process.env.NODE_ENV ?? "development", // or production
-  PORT: process.env.PORT ?? 5000,
+  PORT: process.env.PORT ?? 8080,
   DATABASE_URL: process.env.DATABASE_URL,
 
   JWT_SECRET:process.env.JWT_SECRET ?? ""
