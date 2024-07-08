fb098f33b463189d9c3d25912a0024e9d9b1e3a4
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-08 07:01:37 -0700

    removed validations

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,7 +1,8 @@
 import { body } from "express-validator";
 
 export const registrationSchema = [
-  body("email").trim().isEmail(),
+  // body("email").trim().isEmail(),
+  body("email").trim().isLength({min:1}),
   body("firstName").trim().isLength({ min: 1 }),
   body("lastName").trim().isLength({ min: 1 }),
   body("password").trim().isLength({min:1}),
